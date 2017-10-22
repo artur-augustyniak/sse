4a8f30631f55380223c189fb081832e9af9d6842
Artur Augustyniak <artur@aaugustyniak.pl>
2017-10-22 17:59:26 +0200

    inline printout

--- src/sse_tout.c
+++ src/sse_tout.c
@@ -7,7 +7,7 @@
 #define SSE_N 48 // N % 16 == 0
 
 void loop_sum(void) {
-
+    printf("[+] void loop_sum(void)\n");
     int a[STACK_N];
     memset(a, -1, STACK_N * sizeof (int));
     int sum = 0;
@@ -15,11 +15,10 @@ void loop_sum(void) {
         sum += a[i];
     }
     printf("Loop sum: %d\n", sum);
-
 }
 
 void sse_arr_sum(void) {
-
+    printf("[+] void sse_arr_sum(void)\n");
     int a[4] = {1, 2, 3, 4};
     __m128i* sse_a = (__m128i*) a;
 
@@ -35,13 +34,16 @@ void sse_arr_sum(void) {
 }
 
 void sse_fold_big_arr(void) {
-
+    printf("[+] void sse_fold_big_arr(void)\n");
     int *a = (int*) aligned_alloc(16, SSE_N * sizeof (int));
-    // memset(a, 1, SSE_N * 1);
+    memset(a, 1, SSE_N * 1);
     // or set properly 
     for (int j = 0; j < SSE_N; j++) {
-        a[j]= 1;
+        //a[j]= 1;
+        printf("a[%i] = %i, ", j, a[j]);
     }
+    printf("\n");
+    
    
     const __m128i vk0 = _mm_set1_epi8(0); // constant vector of all  8 bit 0s for use with _mm_unpacklo_epi8/_mm_unpackhi_epi8
     const __m128i vk1 = _mm_set1_epi16(1); // constant vector of all 16 bit 1s for use with _mm_madd_epi16
